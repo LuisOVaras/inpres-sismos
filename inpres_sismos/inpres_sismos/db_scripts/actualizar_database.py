f08bfcb6f1c395ce217110f5220dd0963ff76e46
Luis Varas <96844985+LuisOVaras@users.noreply.github.com>
2024-12-08 08:17:06 -0300

    Update actualizar_database.py

--- inpres_sismos/inpres_sismos/db_scripts/actualizar_database.py
+++ inpres_sismos/inpres_sismos/db_scripts/actualizar_database.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import sqlite3
+import os
+
+# Obtener la ruta absoluta de la ubicación actual del archivo
+base_dir = os.path.dirname(os.path.abspath(__file__))
+csv_path = os.path.join(base_dir, '..', '..', '..', 'data', 'sismos.csv')
+db_path = os.path.join(base_dir, '..', '..', '..', 'data', 'sismos.db')
 
 # Leer el CSV con los datos nuevos (asumo que ya tienes los nuevos datos listos en 'sismos_nuevos.csv')
-sismos_nuevos_df = pd.read_csv("../../../data/sismos.csv")
+sismos_nuevos_df = pd.read_csv(csv_path)
 
 # Convertir la columna 'fecha' de 'DD/MM/YYYY' a 'YYYY-MM-DD'
 sismos_nuevos_df['fecha'] = pd.to_datetime(sismos_nuevos_df['fecha'], format='%d/%m/%Y').dt.strftime('%Y-%m-%d')
@@ -10,7 +16,7 @@ sismos_nuevos_df['fecha'] = pd.to_datetime(sismos_nuevos_df['fecha'], format='%d
 sismos_nuevos_df['sentido'] = sismos_nuevos_df['sentido'].apply(lambda x: 1 if x == 'Si' else 0)
 
 # Crear la conexión a SQLite
-conn = sqlite3.connect("../../../data/sismos.db")
+conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 
 # Revertir el orden de los datos nuevos para que los más recientes queden al final
