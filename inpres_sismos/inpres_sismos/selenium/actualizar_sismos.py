5a548cdaa18589297bba6abad5b9038ebd71a7e5
Luis Varas <vluis2386@gmail.com>
2026-02-14 00:31:57 -0300

    add: webdriver update

--- inpres_sismos/inpres_sismos/selenium/actualizar_sismos.py
+++ inpres_sismos/inpres_sismos/selenium/actualizar_sismos.py
@@ -163,6 +163,10 @@ def main():
     # Configurar Selenium
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()), options=options
     )
